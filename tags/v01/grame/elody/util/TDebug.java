f02ded7cbe87a56b1f15d563bf339e9279aeb9e5
thiolliere <thiolliere@24ffd016-8a14-0410-89a8-9ec6ae8e91d8>
2006-07-12 10:18:25 +0000

    revert tag v02 failed
    git-svn-id: svn+ssh://nina.grame.fr/home/svn/Elody@48 24ffd016-8a14-0410-89a8-9ec6ae8e91d8

--- tags/v01/grame/elody/util/TDebug.java
+++
@@ -1,9 +0,0 @@
-package grame.elody.util;
-
-public class TDebug {
-	static boolean debug = false;
-	static int lastDate = 0;
-	
-	static void On () 	{ debug = true;}
-	static void Off () 	{ debug = false;}
-}
