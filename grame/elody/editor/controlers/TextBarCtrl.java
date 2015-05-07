d7edbd759baf952e217327d3c3d67ecad6d5c3d1
fober <fober@24ffd016-8a14-0410-89a8-9ec6ae8e91d8>
2015-05-07 13:25:38 +0000

    MPL license added as header
    git-svn-id: svn+ssh://nina.grame.fr/home/svn/Elody@104 24ffd016-8a14-0410-89a8-9ec6ae8e91d8

--- grame/elody/editor/controlers/TextBarCtrl.java
+++ grame/elody/editor/controlers/TextBarCtrl.java
@@ -1,3 +1,16 @@
+/*
+  Elody
+  Copyright (C) Grame
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+
+*/
+
 package grame.elody.editor.controlers;
 
 import java.awt.BorderLayout;
